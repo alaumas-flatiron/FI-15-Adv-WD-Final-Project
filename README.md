5a3c4cba2a3688336fc34f006ae503fca3bef32e
emmaladouceur <emmakatherine@optonline.net>
2015-07-29 18:54:07 +0000

    july 29 code

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ MDIwMDA2OTAyMDE0MzgxODI1Mjk2OThmZg001 api key for NPR
 http://api.npr.org/query?id=1007,3&numResults=10&apiKey=MDIwMDA2OTAyMDE0MzgxODI1Mjk2OThmZg001&output=json
 
 list.story[9].text.paragraph.$text
+
+#bdc3c7 = silver color
+
+RGB FORMAT CONVERT TO HEX CODE
+slate 205 215 214
+chocolate 179 163 148
+navy 16 37 66
+coral 248 112 96
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+//http://emmaladouceur-ta-summer2015-110752.nitrousapp.com:3000/
+
 $(document).ready(function(){
   
   
@@ -5,12 +7,17 @@ $(document).ready(function(){
            
 //            var article_title = result. ;
 //            var article_author = result. ;
-              var paragraph = result.list.story[9].text.paragraph;
+              var paragraph = result.list.story[0].text.paragraph;
+    
     var i = 0;
     for (i = 0; i<= paragraph.length; i++) {
-         var article_story = result.list.story[9].text.paragraph[i].$text;
-      console.log(article_story);
+         var article_story = result.list.story[0].text.paragraph[i].$text;
+         console.log(article_story);
+         $('#article-paragraph').append("<p>" + article_story + "</p>");
     };
+    
+     
+    
     
            // defines vars in terms of JSON
     
@@ -20,7 +27,7 @@ $(document).ready(function(){
            // prints vars to console to make sure they are right vals
     
 //            $('#article-heading').html(article_title + "by" + article_author)
-            $('#article-div').html(article_story);
+           
            // changes html of divs with info in JSON
     
    // see if we can set a var = to article = using JSON, get url of article, and pass that into get function
