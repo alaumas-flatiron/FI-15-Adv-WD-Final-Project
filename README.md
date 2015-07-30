791a7c3d3851221ea1b00290aad51c47b27f2ce2
emmaladouceur <emmakatherine@optonline.net>
2015-07-30 18:40:31 +0000

    july 30 edits

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 API KEY  = 76380357c675c74b5d5328157ec1ba35:16:72582646
 
 
-MDIwMDA2OTAyMDE0MzgxODI1Mjk2OThmZg001 api key for NPR
-
-http://api.npr.org/query?id=1007,3&numResults=10&apiKey=MDIwMDA2OTAyMDE0MzgxODI1Mjk2OThmZg001&output=json
+MDIwMDA2OTAyMDE0MzgxODI1Mjk2OThmZg001 = api key for NPR
 
 list.story[9].text.paragraph.$text
 
@@ -13,3 +11,8 @@ chocolate 179 163 148 = #B3A394
 navy 16 37 66   = #102542
 colral 248 112 96 = #F87060
 RGB FORMAT CONVERT TO HEX CODE
+
+http://api.npr.org/query?id=1001&numResults=10&apiKey=MDIwMDA2OTAyMDE0MzgxODI1Mjk2OThmZg001&output=json
+
+http://api.npr.org/query?id=1003&numResults=10&apiKey=MDIwMDA2OTAyMDE0MzgxODI1Mjk2OThmZg001&output=json
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,84 @@
 //http://emmaladouceur-ta-summer2015-110752.nitrousapp.com:3000/
 
 $(document).ready(function(){
+
+            )
   
   
-  $.getJSON('http://api.npr.org/query?id=1001&numResults=10&apiKey=MDIwMDA2OTAyMDE0MzgxODI1Mjk2OThmZg001&output=json', function(result) {
+  
+  $('#a-active').click(function() {
+    
+    $('#article-heading').html(" ");
+   $('#article-paragraph').html(" ");
+    
+  //    OBAMA SUPER COMPUTER HOME PAGE GENERAL NEWS
+$.getJSON('http://api.npr.org/query?id=1001&meta=inherit&apiKey=MDIwMDA2OTAyMDE0MzgxODI1Mjk2OThmZg001&output=json', function(result) {
            
-//            var article_title = result. ;
-//            var article_author = result. ;
+              var news_title = result.list.story[0].title.$text;
+              var news_author = result.list.story[0].byline[0].name.$text;
               var paragraph = result.list.story[0].text.paragraph;
+    // defines vars in terms of JSON
+    
+    console.log(news_title);
+    console.log(news_author);
+ 
+    $('#article-heading').append("<h1>" + news_title + "</h1>");
+    $('#article-heading').append("<h3> by " + news_author + "</h3>");
+    
+    // changes html of divs with info in JSON
     
     var i = 0;
     for (i = 0; i<= paragraph.length; i++) {
          var article_story = result.list.story[0].text.paragraph[i].$text;
          console.log(article_story);
          $('#article-paragraph').append("<p>" + article_story + "</p>");
+               
     };
-    
+   });
+  });             
      
-
+       
+ $('#us').click(function(){
+   
+   $('#article-heading').html(" ");
+   $('#article-paragraph').html(" ");
+    
+  $.getJSON('http://api.npr.org/query?id=1003&meta=inherit&apiKey=MDIwMDA2OTAyMDE0MzgxODI1Mjk2OThmZg001&output=json', function(result){
+   
+    var us_title = result.list.story[1].title.$text;
+    var us_author = result.list.story[1].byline[0].name.$text;
+    var us_paragraph = result.list.story[1].text.paragraph;
 
    
+    var i = 0;
     
-    
-           // defines vars in terms of JSON
-    
-//            console.log(article_title);
-//            console.log(article_author);
-           // console.log(article_story);
-           // prints vars to console to make sure they are right vals
-    
-//            $('#article-heading').html(article_title + "by" + article_author)
-           
-           // changes html of divs with info in JSON
-    
-   // see if we can set a var = to article = using JSON, get url of article, and pass that into get function
+     var full_us_story = function(){
+     for (i = 0; i<= us_paragraph.length; i++) {
+         var us_story = result.list.story[1].text.paragraph[i].$text;
+         console.log(us_story);
+         $('#article-paragraph').append("<p>" + us_story + "</p>");
+    };
+     };
+   
+  $('#article-heading').append("<h1>" + us_title + "</h1>");
+    $('#article-heading').append("<h3> by " + us_author + "</h3>");
+  ('#article-paragraph').html("<p>" + full_us_story() + "</p>");
+
+});
+ 
+
+});
   
   });
   
+ 
+
+
+
+
   
+   
+ 
 
   
   
@@ -52,5 +93,3 @@ $(document).ready(function(){
   
   
   
-  
-});
